3e18a41d86da718f6c708be5f418a336740aa9a9
rizalsyafrudin <115907941+rizalsyafrudin@users.noreply.github.com>
2022-11-24 18:57:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -378,7 +378,7 @@ global.hwaifu = ['https://i.pinimg.com/originals/ed/34/f8/ed34f88af161e6278993e1
 'https://i.pinimg.com/originals/fd/21/41/fd21419275236bb153de3c8dcbbf3bf9.jpg',
 'https://i.pinimg.com/originals/80/4f/1a/804f1a05f9996c96a2d492b4854b7fd5.jpg']
 /*============== STICKER WM ==============*/
-global.stickpack = '"Malix_Project"\n"${msg.pushName}"\n"owner": "wa.me/6283152615606"\n"ig": "@cowok_kul_'
+global.stickpack = '"Malix_Project"\n${msg.pushName}\n"owner": "wa.me/6283152615606"\n"ig": "@cowok_kul_'
 global.stickauth = `M'\n'A'\n'L'\n'I'\n'X'`
 
 global.multiplier = 38 // The higher, The harder levelup
