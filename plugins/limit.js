27c119b369b61b5de58f4fbc21f81fe34148055b
rizalsyafrudin <115907941+rizalsyafrudin@users.noreply.github.com>
2022-11-25 10:29:55 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-    m.reply(`${global.db.data.users[who].limit} Limit Left=͟͟͞͞🏀`)
+    m.reply(`${global.db.data.users[who].limit} Jumlah limit tersisa
 }
 handler.help = ['limit [@user]']
 handler.tags = ['xp']
