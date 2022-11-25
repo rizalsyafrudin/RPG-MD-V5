2a0b9bc4974a61fc7bd8227a1c9a97e1d45f1434
rizalsyafrudin <115907941+rizalsyafrudin@users.noreply.github.com>
2022-11-25 08:23:09 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -186,9 +186,9 @@ let handler = async (m, { conn, usedPrefix, __dirname }) => {
 `, text.trim(), flaa, [
       [`ꜱᴇᴡᴀʙᴏᴛ`, `${usedPrefix}sewa`],
       [`۪۪ᴀᴜᴛʜᴏʀ ʙᴏᴛ`, `${usedPrefix}owner`]
-    ], m, { 'document':{'url':'https://youtu.be/hklv-Ysqqac'},
+    ], m, { 'document':{'url':'https://youtu.be/DCfk7tc_KqE'},
 'mimetype':global.dpdf,
-'fileName':'「 ᴹᴿ᭄ King Of Bear ×፝֟͜× 」',
+'fileName':' Malix_Project ',
 'fileLength':fsizedoc,
 'pageCount':fpagedoc,
 'previewType':'pdf',
@@ -197,9 +197,9 @@ contextInfo: { externalAdReply :{ showAdAttribution: true,
                         mediaType: 2,
                         description: `⚘ ᴄʀᴇᴀᴛᴇ ᴹᴿ᭄ King Of Bear ×፝֟͜×`,
                         title: `💌 My Ultah: ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `⚘ by ᴹᴿ᭄ King Of Bear ×፝֟͜×`,          previewType: 0,
+                        body: `⚘ by Malix_project`,          previewType: 0,
                         thumbnail: await (await fetch(thumb)).buffer(),
-                        mediaUrl: 'https://youtu.be/hklv-Ysqqac'
+                        mediaUrl: 'https://youtu.be/DCfk7tc_KqE'
                         
                       }}
 })
