e052aa55ee4d02fbdc6c974353cb55f8c97d2392
rizalsyafrudin <115907941+rizalsyafrudin@users.noreply.github.com>
2022-11-25 09:34:44 +0700

    Update rpg-freegift.js

--- plugins/rpg-freegift.js
+++ plugins/rpg-freegift.js
@@ -4,10 +4,10 @@ let handler = async(m, { conn, args, usedPrefix }) => {
     if (args[0] == 'fangzbot_091' || args[0] == 'followfangz_' || args[0] == 'BloowwXx' || args[0] == 'BbL016JJQBCSr54OwwW0' || args[0] == 'giftkey01389320007' || args[0] == 'kode013923') {
 
     if (new Date - global.db.data.users[m.sender].lastgift > 86400000) {
-       conn.reply(m.chat, '*🎉 SELAMAT!*\nKamu telah mendapatkan\n1000 XP ✨\n 1 Limit! 🎫\n1000 Money 💹\n1 Potion 🥤', m)
+       conn.reply(m.chat, '*🎉 SELAMAT!*\nKamu telah mendapatkan\n2000000 XP ✨\n 1 Limit! 🎫\n100000000 Money 💹\n1 Potion 🥤', m)
     global.db.data.users[m.sender].exp += 2000000
     global.db.data.users[m.sender].limit += 1
-    global.db.data.users[m.sender].money +=1000
+    global.db.data.users[m.sender].money +=100000000
     global.db.data.users[m.sender].potion += 1
     global.db.data.users[m.sender].lastclaim = new Date * 1
 } else conn.reply(m.chat, '[❗] Kode Gift Gratis hanya dapat digunakan sehari sekali ! dan kode hanya bisa di pakai sekali !\n\nKetik *!buygift* untuk membeli kodegift premium', m)
